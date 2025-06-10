5f47b6249c8df3614bf38985d7867ddf1ff59c51
Alex Tan Hong Pin <alextan220990@gmail.com>
2025-06-10 22:38:40 +0800

    chore: simplify type assertions

--- cause/validation.go
+++ cause/validation.go
@@ -1,7 +1,6 @@
 package cause
 
 import (
-	"cmp"
 	"errors"
 	"fmt"
 	"maps"
@@ -14,35 +13,6 @@ var validatableType = reflect.TypeOf((*validatable)(nil)).Elem()
 
 type validatable interface {
 	Validate() error
-}
-
-type next interface {
-	Next() error
-}
-
-func SliceFunc[T any](vs []T, fn func(T) error) sliceValidator {
-	res := make([]validatable, len(vs))
-	for i, v := range vs {
-		res[i] = &validator{
-			err: fn(v),
-		}
-	}
-
-	return res
-}
-
-type errorMap map[string]any
-
-func (e errorMap) Map() map[string]any {
-	return e
-}
-
-func (e errorMap) Error() string {
-	return "invalid fields: " + e.String()
-}
-
-func (e errorMap) String() string {
-	return strings.Join(slices.Sorted(maps.Keys(e)), ", ")
 }
 
 type Map map[string]any
@@ -63,25 +33,27 @@ func (m Map) Err() error {
 					errs = errs[1:]
 
 					switch e := err.(type) {
-					case *errorMulti:
-						errs = append(errs, e.Errors()...)
-					case errorSliceIndex:
-						for i, ve := range e {
-							em[fmt.Sprintf("%s[%d]", k, i)] = ve
-						}
-					case errorMap:
-						em[k] = e
-					case error:
-						em[k] = e.Error()
+					case errorMulti:
+						errs = append(errs, e...)
+					case errorIndex:
+						em[fmt.Sprintf("%s[%d]", k, e.pos)] = e.err
 					default:
 						em[k] = e
 					}
 				}
 			}
-		case error:
-			em[k] = t.Error()
 		default:
 			em[k] = v
+		}
+	}
+
+	for k, v := range em {
+		switch e := v.(type) {
+		case errorMulti, errorIndex, errorMap:
+			// Do nothing, we already have the error in the map.
+		case error:
+			// If it's a single error, we can just use it directly.
+			em[k] = e.Error()
 		}
 	}
 	if len(em) == 0 {
@@ -98,15 +70,15 @@ func Optional(val any, msgs ...string) *Builder {
 
 	if v, ok := isSlice(val); ok {
 		return &Builder{
-			v:    v,
 			msgs: msgs,
+			v:    v,
 		}
 	}
 
 	if v, ok := val.(validatable); ok {
 		return &Builder{
-			v:    v,
 			msgs: msgs,
+			v:    v,
 		}
 	}
 
@@ -117,24 +89,6 @@ func Optional(val any, msgs ...string) *Builder {
 
 func Required(val any, msgs ...string) *Builder {
 	return RequiredMessage(val, "required", msgs...)
-}
-
-func isSlice(t any) (sliceValidator, bool) {
-	v := reflect.ValueOf(t)
-	if v.Kind() == reflect.Slice {
-		result := make([]validatable, v.Len())
-		for i := range v.Len() {
-			e, ok := v.Index(i).Interface().(validatable)
-			if !ok {
-				return nil, false
-			}
-			result[i] = e
-		}
-
-		return result, true
-	}
-
-	return nil, false
 }
 
 func RequiredMessage(val any, msg string, msgs ...string) *Builder {
@@ -163,6 +117,17 @@ func RequiredMessage(val any, msg string, msgs ...string) *Builder {
 	}
 }
 
+func SliceFunc[T any](vs []T, fn func(T) error) sliceValidator {
+	res := make([]validatable, len(vs))
+	for i, v := range vs {
+		res[i] = &validator{
+			err: fn(v),
+		}
+	}
+
+	return res
+}
+
 type Builder struct {
 	msgs []string
 	v    validatable
@@ -185,20 +150,54 @@ func (b *Builder) Validate() error {
 		return nil
 	}
 
-	em := new(errorMulti)
+	var errs errorMulti
 	if msg := joinStrings(b.msgs...); msg != "" {
-		em.err1 = errors.New(msg)
+		errs = append(errs, errors.New(msg))
 	}
 
 	if b.v != nil {
-		em.err2 = b.v.Validate()
+		if err := b.v.Validate(); err != nil {
+			errs = append(errs, err)
+		}
 	}
 
-	if em.Unwrap() != nil {
-		return em
+	if len(errs) == 0 {
+		return nil
 	}
 
-	return nil
+	return errs
+}
+
+type errorMap map[string]any
+
+func (e errorMap) Map() map[string]any {
+	return e
+}
+
+func (e errorMap) Error() string {
+	return "invalid fields: " + e.String()
+}
+
+func (e errorMap) String() string {
+	return strings.Join(slices.Sorted(maps.Keys(e)), ", ")
+}
+
+func isSlice(t any) (sliceValidator, bool) {
+	v := reflect.ValueOf(t)
+	if v.Kind() == reflect.Slice {
+		result := make([]validatable, v.Len())
+		for i := range v.Len() {
+			e, ok := v.Index(i).Interface().(validatable)
+			if !ok {
+				return nil, false
+			}
+			result[i] = e
+		}
+
+		return result, true
+	}
+
+	return nil, false
 }
 
 func isZero(t any) bool {
@@ -238,53 +237,30 @@ func (v *validator) Validate() error {
 	return v.err
 }
 
-type errorSliceIndex map[int]any
+type errorIndex struct {
+	pos int
+	err error
+}
 
-func (es errorSliceIndex) Error() string {
-	return "invalid slice"
+func (ei errorIndex) Error() string {
+	return fmt.Sprintf("error at index %d: %s", ei.pos, ei.err.Error())
 }
 
 type sliceValidator []validatable
 
 func (s sliceValidator) Validate() error {
-	es := make(errorSliceIndex)
+	var em errorMulti
 	for i, v := range s {
 		if err := v.Validate(); err != nil {
-			switch e := err.(type) {
-			case errorSliceIndex, errorMap:
-				es[i] = e
-			case error:
-				es[i] = e.Error()
-			default:
-				es[i] = e
-			}
+			em = append(em, errorIndex{i, err})
 		}
 	}
 
-	return es
+	return em
 }
 
-type errorMulti struct {
-	err1 error
-	err2 error
-}
+type errorMulti []error
 
-func (e *errorMulti) Error() string {
-	return errors.Join(e.err1, e.err2).Error()
-}
-
-func (e *errorMulti) Errors() []error {
-	var errs []error
-	if e.err1 != nil {
-		errs = append(errs, e.err1)
-	}
-	if e.err2 != nil {
-		errs = append(errs, e.err2)
-	}
-
-	return errs
-}
-
-func (e *errorMulti) Unwrap() error {
-	return cmp.Or(e.err1, e.err2)
+func (e errorMulti) Error() string {
+	return errors.Join(e).Error()
 }
